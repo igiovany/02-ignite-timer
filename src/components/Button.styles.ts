d1c4ee2380615fa55b303d8bd8c217e1ee8c7c37
Giovany Lawall Inocêncio <ino.giovany@gmail.com>
2022-10-25 21:16:43 -0300

    Global styles and default theme

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,10 +16,16 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
+    border-radius: 4px;
+    border: 0;
+    margin: 0.5rem;
 
-    ${props => {
+    background-color: ${props => props.theme["green-500"]};
+
+    /* ${props => {
         return css`
-            background-color: ${buttonVariants[props.variant]}
+            background-color: ${buttonVariants[props.variant]};
         `
-    }}
+    }} */
+    
 `
